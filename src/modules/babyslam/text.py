1d2e75ad20f5109c7bbe8253970f2d69598a61fb
Karel Vervaeke <karel@rolmops.(none)>
2010-01-25 09:04:11 +0100

    readded missing media files and fixed text.py
    const module was renamed to shared, but text.py was not updated accordingly

--- src/modules/babyslam/text.py
+++ src/modules/babyslam/text.py
@@ -1,15 +1,15 @@
 from pygame.locals import *
 
-import const
+import shared
 
-def drawText(text, font, surface, x, y, color = const.TEXTCOLOR, center = False):
+def drawText(text, font, surface, x, y, color = shared.TEXTCOLOR, center = False):
     textobj = font.render(text, 1, color)
     if center:
       x,y = x-textobj.width/2,y-textobj.height/2
     surface.blit(textobj, (x, y))
 
 # cheap but effective way to render 1-pixel outlines
-def drawTextOutline(text, font, surface, x, y, color = const.TEXTCOLOR, outline_color = const.OUTLINECOLOR, center = False):
+def drawTextOutline(text, font, surface, x, y, color = shared.TEXTCOLOR, outline_color = shared.OUTLINECOLOR, center = False):
     base_out = font.render(text, 0, outline_color)
     base_in = font.render(text, 0, color)
     if center:
@@ -20,6 +20,6 @@ def drawTextOutline(text, font, surface, x, y, color = const.TEXTCOLOR, outline_
     surface.blit(base_out, (x+1,y+1))
     surface.blit(base_in, (x,y))
 
-def createText(text, font, color = const.TEXTCOLOR):
+def createText(text, font, color = shared.TEXTCOLOR):
     return font.render(text, 0, color)
 
